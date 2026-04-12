16ef1d332f22b425016e0f9d5b78161abbb853c7
Nikolayuk_v <Nistor2009@yandex.ru>
2026-04-12 19:24:53 +0300

    3 домашка по ООП

--- OOP.py
+++ OOP.py
@@ -1,8 +1,24 @@
-class Pet:
-    def __init__(self, name, age, master):
+import abc
+from abc import ABC
+
+class NonNegative:
+    def __get__(self, instance, owner):
+        return instance.__dict__[self.name]
+    def __set__(self, instance, value):
+        if value <= 0:
+            raise ValueError('Cannot be negative.')
+        instance.__dict__[self.name] = value
+    def __set_name__(self, owner, name):
         self.name = name
+
+class Pet(ABC):
+    age = NonNegative()
+    __default_name = 'default name'
+    __default_master = 'default master'
+    def __init__(self, name, age, master):
+        self.__name = name if not name is None else self.__default_name
         self.age = age
-        self.__master = master
+        self.__master = master if not master is None else self.__default_master
     def jump(self):
         print(f"jump")
     def run(self):
@@ -11,8 +27,27 @@ class Pet:
         self.age += 1
     def sleep(self):
         print(f"sleep")
-    def get_master(self):
+    @property
+    def master(self):
         return self.__master
+    @master.setter
+    def master(self, value):
+        if value is None:
+            self.__master = self.__default_master
+        else:
+            self.__master = value
+    @property
+    def name(self):
+        return self.__name
+    @name.setter
+    def name(self, value):
+        if value is None:
+            value = self.__default_name
+        else:
+            self.__name = value
+    @abc.abstractmethod
+    def voice(self):
+        pass
 
 #HT_14_1
 class Dog(Pet):
@@ -20,46 +55,51 @@ class Dog(Pet):
         self.height = height
         self.weight = weight
         Pet.__init__(self, name, age, master)
-
-    def bark(self):
+    def __bark(self):
         print(f"bark")
     def change_name(self, new_name):
         self.name = new_name
-dog  = Dog(1,2,'Dog', 4, 'Human')
+    def voice(self):
+        self.__bark()
+
+dog  = Dog(1,2,'Dog', 4, None)
 dog.jump()
 dog.run()
-dog.bark()
+dog.voice()
 dog.sleep()
 print(dog.height)
 print(dog.weight)
 print(dog.name)
 print(dog.age)
+print(dog.master)
 
 #HT14_2
 dog.change_name('New_dog')
 print(dog.name)
 
-#14_4
-print(dog.get_master())
-
 #14.5
 class Cat(Pet):
     def __init__(self, name, age, master):
         Pet.__init__(self, name, age, master)
-    def meow(self):
+    def __meow(self):
         print(f"meow")
+    def voice(self):
+        self.__meow()
 
 class Parrot(Pet):
     def __init__(self, name, age, master):
         Pet.__init__(self, name, age, master)
     def fly(self):
         print(f"fly")
+    def voice(self):
+        print(f"piu")
 
 
 
 #14.6
-cat = Cat("Cat", 1, 'Cat_human')
-cat.meow()
+cat = Cat(None, 1, 'Cat_human')
+print(cat.name)
+cat.voice()
 cat.jump()
 cat.run()
 cat.birthday()
@@ -68,6 +108,7 @@ print(cat.age)
 parrot = Parrot("Parrot", 2, 'Parrot_human')
 parrot.fly()
 parrot.jump()
+parrot.voice()
 parrot.run()
 parrot.birthday()
 print(parrot.age)
